c56a60b26ed066c7c5544c050eae4452be127a8b
Adam Roberts <adam.roberts@bbc.co.uk>
2025-01-17 14:24:12 +0000

    adding task 3 code

---
+++ adam-workshop/lambda/authors.js
@@ -0,0 +1,29 @@
+const authors = [
+  {
+    id: 1,
+    name: "Jane Austen",
+    genre: "Fiction",
+    nationality: "British",
+  },
+  {
+    id: 2,
+    name: "Mark Twain",
+    genre: "Fiction",
+    nationality: "American",
+  },
+  {
+    id: 3,
+    name: "Toni Morrison",
+    genre: "Literary Fiction",
+    nationality: "American",
+  },
+];
+
+exports.handler = async function (event) {
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: authors,
+  };
+};
