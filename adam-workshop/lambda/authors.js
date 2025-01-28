165d22bda963a0d77d9cce59f3b90ca0f98216c7
Adam Roberts <adam.roberts@bbc.co.uk>
2025-01-28 08:00:08 +0000

    stringifying authors body

--- adam-workshop/lambda/authors.js
+++ adam-workshop/lambda/authors.js
@@ -24,6 +24,6 @@ exports.handler = async function (event) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: authors,
+    body: JSON.stringify(authors),
   };
 };
