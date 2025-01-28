fa52c9484acd40f13332d496bc9780c68e2779de
Adam Roberts <adam.roberts@bbc.co.uk>
2025-01-28 07:41:37 +0000

    adding task 4 code

--- adam-workshop/lib/adam-workshop-stack.js
+++ adam-workshop/lib/adam-workshop-stack.js
@@ -1,5 +1,6 @@
 const { Stack } = require("aws-cdk-lib");
 const lambda = require("aws-cdk-lib/aws-lambda");
+const apigateway = require("aws-cdk-lib/aws-apigateway");
 
 class AdamWorkshopStack extends Stack {
   constructor(scope, id, props) {
@@ -12,6 +13,16 @@ class AdamWorkshopStack extends Stack {
       functionName: "AdamAuthorsFunction",
       handler: "authors.handler",
     });
+
+    // Create the API Gateway
+    const api = new apigateway.RestApi(this, "AdamBookshopApi", {
+      restApiName: "AdamBookshopService",
+      description: "This service serves bookshop data.",
+    });
+
+    // Create the /authors route and connect it to our Authors Lambda function
+    const authors = api.root.addResource("authors");
+    authors.addMethod("GET", new apigateway.LambdaIntegration(authorsFunction));
   }
 }
 
