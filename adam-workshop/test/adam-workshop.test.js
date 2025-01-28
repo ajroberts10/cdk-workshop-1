5a24ebd834c676c043a371ed2b736d7b22f61539
Adam Roberts <adam.roberts@bbc.co.uk>
2025-01-28 07:41:37 +0000

    adding task 4 code

--- adam-workshop/test/adam-workshop.test.js
+++ adam-workshop/test/adam-workshop.test.js
@@ -13,3 +13,26 @@ test("Author Lambda Function Created", () => {
     FunctionName: "AdamAuthorsFunction",
   });
 });
+
+test("API Gateway Created with /authors Route and Lambda Integration", () => {
+  const app = new cdk.App();
+
+  const stack = new AdamWorkshop.AdamWorkshopStack(app, "MyTestStack");
+
+  const template = Template.fromStack(stack);
+
+  // Assert that the /authors resource exists and is integrated with a Lambda function
+  template.hasResourceProperties("AWS::ApiGateway::Resource", {
+    PathPart: "authors",
+  });
+
+  // Check that the GET method for /authors is using Lambda integration
+  template.hasResourceProperties("AWS::ApiGateway::Method", {
+    AuthorizationType: "NONE",
+    HttpMethod: "GET",
+    Integration: {
+      IntegrationHttpMethod: "POST",
+      Type: "AWS_PROXY", // AWS_PROXY is the integration type for Lambda functions
+    },
+  });
+});
