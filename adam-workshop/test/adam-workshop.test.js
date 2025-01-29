d34fc61033d99b9235f9bc7c13c1e24e8169d808
Adam Roberts <adam.roberts@bbc.co.uk>
2025-01-29 17:39:56 +0000

    adding task 6 code

--- adam-workshop/test/adam-workshop.test.js
+++ adam-workshop/test/adam-workshop.test.js
@@ -2,7 +2,7 @@ const cdk = require("aws-cdk-lib");
 const { Template } = require("aws-cdk-lib/assertions");
 const AdamWorkshop = require("../lib/adam-workshop-stack");
 
-test("Author Lambda Function Created", () => {
+test("Authors Lambda Function Created", () => {
   const app = new cdk.App();
 
   const stack = new AdamWorkshop.AdamWorkshopStack(app, "MyTestStack");
@@ -11,6 +11,18 @@ test("Author Lambda Function Created", () => {
 
   template.hasResourceProperties("AWS::Lambda::Function", {
     FunctionName: "AdamAuthorsFunction",
+  });
+});
+
+test("Books Lambda Function Created", () => {
+  const app = new cdk.App();
+
+  const stack = new AdamWorkshop.AdamWorkshopStack(app, "MyTestStack");
+
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    FunctionName: "AdamBooksFunction",
   });
 });
 
@@ -24,6 +36,30 @@ test("API Gateway Created with /authors Route and Lambda Integration", () => {
   // Assert that the /authors resource exists and is integrated with a Lambda function
   template.hasResourceProperties("AWS::ApiGateway::Resource", {
     PathPart: "authors",
+  });
+
+  // Check that the GET method for /authors is using Lambda integration
+  template.hasResourceProperties("AWS::ApiGateway::Method", {
+    AuthorizationType: "NONE",
+    ApiKeyRequired: true,
+    HttpMethod: "GET",
+    Integration: {
+      IntegrationHttpMethod: "POST",
+      Type: "AWS_PROXY", // AWS_PROXY is the integration type for Lambda functions
+    },
+  });
+});
+
+test("API Gateway Created with /books Route and Lambda Integration", () => {
+  const app = new cdk.App();
+
+  const stack = new AdamWorkshop.AdamWorkshopStack(app, "MyTestStack");
+
+  const template = Template.fromStack(stack);
+
+  // Assert that the /authors resource exists and is integrated with a Lambda function
+  template.hasResourceProperties("AWS::ApiGateway::Resource", {
+    PathPart: "books",
   });
 
   // Check that the GET method for /authors is using Lambda integration
